ed45f855a626ebe717259c7fd85f28c2dbd6966b
abbhagat27@gmail.com <Dante@475812>
2026-04-04 15:55:27 +0530

    Updates

--- assets/arrow-right-1L768qWX.js
+++ assets/arrow-right-1L768qWX.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-DgKiQy_k.js";/**
+import{c as o}from"./index-B0Wum2iT.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
