fdb533d1ea8927eaad3da20a08770d260427a51e
abbhagat27@gmail.com <Dante@475812>
2026-04-14 20:28:43 +0530

    refactor: remove obsolete project files and cleanup repository structure

--- scratch/check_mafs.ts
+++
@@ -1,5 +0,0 @@
-import * as mafs from "mafs";
-console.log("Mafs exports:", Object.keys(mafs));
-console.log("Plot keys:", Object.keys(mafs.Plot));
-console.log("Line keys:", Object.keys(mafs.Line));
-console.log("Theme keys:", Object.keys(mafs.Theme));
